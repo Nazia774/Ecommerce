fab14958bd6f37cdca6a096859b3b68fbdab9021
Nazia Khan <nk9555100@gmail.com>
2026-02-20 12:09:24 +0530

    Initial Ecommerce project

---
+++ src/test/java/pages/test1.java
@@ -0,0 +1,9 @@
+package pages;
+
+public class test1 {
+
+		public static void main(String[] args) {
+			System.out.println("Nazia khan");
+		}
+		}
+
---
+++ src/test/java/tests/test2.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class test2 {
+	
+		public static void main(String[] args) {
+			System.out.println("Nazia khan");
+		}
+		}
+
+
